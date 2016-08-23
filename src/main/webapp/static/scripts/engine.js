e1f92696a209a8b06d45e60561068a4807d26cc7
cristiano-c <cristianocavo@me.com>
2016-08-23 14:45:30 +0200

    Aggiunta la popolazione delle tre variabili

--- src/main/webapp/static/scripts/engine.js
+++ src/main/webapp/static/scripts/engine.js
@@ -1852,6 +1852,11 @@ iftttApp.controller('publicRecipesController', ['$scope', '$routeParams', '$wind
             }, function (isConfirm) {
                 if(isConfirm){
 
+                    // LE TRE VARIABILI *GLOBALI* PER FXR!!
+                    importFlag = true;
+                    triggerImportRoute = getRoute(triggerCode); //Se triggerCode è "14" allora ti ritorna "/WeatherTrigger1" cmq puoi fare degli alert per testare
+                    actionImportRoute = getRoute(actionCode);
+
                     alert("implementare qui le funzioni che permettono di continuare l'importazione della ricetta (engine.js riga 1864 xxx)");
                     alert("devo andare nel trigger form :" + triggerCode);
                     alert("e POI devo andare nell'action form :" + actionCode);
